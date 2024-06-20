64bd6fcaa72a40c0a1f5e66911bf97f39e34e27f
Mikael Weiss <campingmikael@icloud.com>
2024-06-19 22:22:38 -0600

    Added word count and reading time to lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,13 +1,32 @@
 return {
     "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "skwee357/nvim-prose",
+    },
     config = function()
-        require('lualine').setup({
+        local prose = require("nvim-prose")
+
+        require("nvim-prose").setup({
+            wpm = 200.0,
+            filetypes = { "markdown", "text", "tex", "rst", "txt", "swift", "lua" },
+            placeholders = {
+                words = "words",
+                minutes = "min",
+            },
+        })
+
+        require("lualine").setup({
             options = {
-                theme = 'codedark'
-            }
+                theme = "codedark",
+            },
+            sections = {
+                lualine_x = {
+                    { prose.word_count,   cond = prose.is_available },
+                    { prose.reading_time, cond = prose.is_available },
+                },
+            },
         })
         vim.opt.laststatus = 3 -- Always show statusline
         vim.opt.cmdheight = 0 -- Makes it so that command and lualine share the same line
-    end
+    end,
 }
-
